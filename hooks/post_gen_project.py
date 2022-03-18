d31ea78f7ef1c1671db1d2a26ed2616aad7dcccf
Toshio Kuratomi <a.badger@gmail.com>
2022-03-18 14:48:11 -0700

    Reimplement license selection
    * Use SPDX short names to describe the licenses
    * Make a .cookiecutter_data directory in which templates only used
      during baking can be stored (These templates will not be present in
      the cookiecutter output).
    * Re-implement the license generation as separate templates which are
      selected by the SPDX short name.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import shutil
 import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
@@ -13,4 +14,5 @@ if __name__ == "__main__":
     if "Not open source" == "{{ cookiecutter.open_source_license }}":
         remove_file("LICENSE")
 
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, '.cookiecutter_data'))
     subprocess.check_call(["git", "init"])
